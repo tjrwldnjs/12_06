e174ddb4d0719127ad3a6f81a1ba811247663d35
tjrwldnjs <sue06024@naver.com>
2018-12-06 15:27:54 +0900

    3

--- 이름없음2.c
+++ 이름없음2.c
@@ -6,6 +6,8 @@ typedef struct linknd {
 	//struct linknd *next;
 } linknd_t;
 
-
 //struct linknd nd; 이거는 나중에 복잡하니까(typedef 안쓸때) 
-linknd_t nd; //typedef쓰면 이렇게. 간단하게. 
+//linknd_t nd; typedef쓰면 이렇게. 간단하게. (아래참고) 
+
+static linknd_t *list; //linked list 실체 , static은 정적할당 - 다른 파일에서 접근 못하도록. 
+
