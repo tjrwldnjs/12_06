932d57ac20afe82deb0215235a424cc158211404
tjrwldnjs <sue06024@naver.com>
2018-12-06 15:41:35 +0900

    4

--- 이름없음2.c
+++ 이름없음2.c
@@ -1,4 +1,5 @@
-
+#include <stdio.h>
+#include <stdlib.h> 
 typedef struct linknd {
 	
 	int data;
@@ -11,3 +12,22 @@ typedef struct linknd {
 
 static linknd_t *list; //linked list 실체 , static은 정적할당 - 다른 파일에서 접근 못하도록. 
 
+linknd_t* create_node(int value ){
+	
+	linknd_t* ndPtr;
+	
+	// 동적 메모리 할당
+	ndPtr = (linknd_t*)malloc( sizeof(linknd_t) );
+	if (ndPtr == NULL)
+	{
+		printf("ERROR\n");
+		return NULL;
+	}
+	
+	//정수값 저장
+	ndPtr->data = value;
+	ndPtr->next = NULL; //liked list의 노드 초기화. 
+	 
+	 return ndPtr;  
+	 
+} 
